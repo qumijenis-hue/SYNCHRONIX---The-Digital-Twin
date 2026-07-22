a4c602fcdb3aa88801661df0c1f3ccce4eb90794
qumijenis-hue <qumijenis@student.tce.edu>
2026-07-22 14:59:43 +0530

    Add files via upload
    add index.html

---
+++ script1.js
@@ -0,0 +1,7 @@
+document.getElementById("googleLogin").addEventListener("click", () => {
+    alert("Google Sign-In Clicked");
+});
+
+document.getElementById("facebookLogin").addEventListener("click", () => {
+    alert("Facebook Sign-In Clicked");
+});
